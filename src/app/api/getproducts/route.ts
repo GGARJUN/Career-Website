361efe31bd9b21845dacdb6b2ec492db412e8d25
GGARJUN <techarjung@gmail.com>
2024-03-19 17:40:14 +0530

    to

--- src/app/api/getproducts/route.ts
+++ src/app/api/getproducts/route.ts
@@ -1,14 +1,14 @@
-import { NextResponse } from "next/server";
-import Stripe from "stripe";
+// import { NextResponse } from "next/server";
+// import Stripe from "stripe";
 
-export async function GET(request) {
-  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-    apiVersion: "2023-10-16",
-  });
+// export async function GET(request) {
+//   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+//     apiVersion: "2023-10-16",
+//   });
 
-  const prices = await stripe.prices.list({
-    limit: 3,
-  });
+//   const prices = await stripe.prices.list({
+//     limit: 3,
+//   });
 
-  return NextResponse.json(prices.data);
-}
+//   return NextResponse.json(prices.data);
+// }
